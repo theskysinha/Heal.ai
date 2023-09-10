9271340607a77cf893f5b29a656424c54e1652d3
Parkhi Saini <90965313+ParkhiSaini@users.noreply.github.com>
2023-09-11 00:28:26 +0530

    feat:Registration and Login using password

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,33 @@ import Journal from './components/pages/Journal/Journal';
 import ChatBot from './components/pages/ChatBot/ChatBot';
 import Login from './components/pages/Login/Login';
 import Zen from './components/pages/Zen/Zen';
+import Registration from './components/pages/Registration/Registration';
 import { Routes, Route, BrowserRouter as Router} from 'react-router-dom';
+import { Toaster } from 'react-hot-toast'
+import { UserContextProvider } from './Context/userContext';
+
+
+
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.withCredentials =true
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
     <Router>
+        <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
       <Routes>
+       
         <Route path="/" element={<Home />} />
         <Route path="/journal" element={<Journal />} />
         <Route path="/chatbot" element={<ChatBot />} />
         <Route path="/login" element={<Login />} />
         <Route path="/zen" element={<Zen />} />
+        <Route path="/registration" element={<Registration />} />
       </Routes>
     </Router>
-    </>
+    </UserContextProvider>
   );
 }
 
---
+++ server/controllers/authControllers.js
@@ -0,0 +1,103 @@
+const User = require ('../models/User');
+const {hashPassword, comparePassword} = require('../helpers/auth');
+const jwt = require ('jsonwebtoken');
+
+
+const test = (req,res)=>{
+    res.json('test is working')
+}
+
+//Register endpoint
+
+const registerUser = async(req,res)=>{
+    try {   
+        const{email,password}= req.body;
+        //check if name is entered
+        // if(!name)
+        // {
+        //     return res.json({
+        //         error:'name is not required'
+        //     })
+        // };
+        if(!password|| password.length<6)
+        {
+            return res.json({
+                error:'password is required and should be 6 characters long'
+            })
+        };
+        //Check email
+        const exist = await User.findOne({email});
+        if(exist )
+        {
+            return res.json({
+                error:'Email is already taken'
+            })
+        }
+
+        const hashedPassword=await hashPassword(password)
+        //Create user in database
+        const user = await User.create({
+            email,
+            password:hashedPassword
+        })
+
+        return res.json(user)
+    }catch(error){
+        console.log(error)
+
+    }
+
+};
+
+//Login Endpoint
+const loginUser = async (req,res)=>{
+    try{
+        const{email,password} = req.body;
+
+        //Check is user exists
+
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.json({
+                error:'User not found'
+            })
+        }
+
+
+        //Check if password is a match
+        const match = await comparePassword(password,user.password)
+        if(match)
+        {
+            jwt.sign({email:user.email, id:user.id},process.env.JWT_SECRET,{},(err,token)=>{
+                if(err) throw err;
+                res.cookie('token', token).json(user)
+            })
+            // res.json('passwords match')
+        }
+        if(!match)
+        {
+            res.json('passwords dont match')
+        }
+    }catch(error){  
+        console.log(error)
+    }
+}
+
+const  getProfile=(req,res)=>{
+const{token}=req.cookies
+if(token){
+    jwt.verify(token,process.env.JWT_SECRET, {}, (err,user) =>{
+        if (err) throw err;
+        res.json(user)
+    })
+}
+else{
+    res.json(null)
+}
+}
+module.exports={
+    test,
+    registerUser,
+    loginUser,
+    getProfile
+}
